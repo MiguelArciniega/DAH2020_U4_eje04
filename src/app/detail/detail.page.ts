6e00366a7f02d709f0d5fc2dec0275d80ec706e6
Miguel Arciniega <milearciniegara@ittepic.edu.mx>
2021-01-12 03:31:44 -0700

    Reparación de Detalles

---
+++ src/app/detail/detail.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Estudiante } from "../models/estudiante";
+import { EstudianteService } from "../services/estudiante.service";
+import { ToastController } from "@ionic/angular";
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.page.html',
+  styleUrls: ['./detail.page.scss'],
+})
+export class DetailPage implements OnInit {
+
+  student: Estudiante;
+
+  constructor(private service: EstudianteService,
+    private actroute: ActivatedRoute,
+    private router: Router,
+    private toast: ToastController) {
+    this.actroute.queryParams.subscribe(
+      params => {
+        if (params && params.special) {
+          this.student = JSON.parse(params.special) as Estudiante;
+          console.log(this.student);
+        }
+      }
+    );
+  }
+
+  ngOnInit() {
+  }
+
+}
